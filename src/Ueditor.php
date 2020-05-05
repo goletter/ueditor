cf469a2b5233f3070179f574bdbdbde0e2bd2ce5
goletter <goletter@outlook.com>
2020-05-05 14:23:42 +0800

    框架优化

--- src/Ueditor.php
+++ src/Ueditor.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Contracts\Validation\Factory as Validator;
 use Goletter\Ueditor\Contracts\Ueditor as UeditorInteface;
 
+
 class Ueditor implements UeditorInteface
 {
     protected $request;
